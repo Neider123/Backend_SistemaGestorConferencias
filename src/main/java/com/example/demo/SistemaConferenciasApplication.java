d87d253a3d878c20fb84d5e4cdd61e280331938d
Neider123 <neiderjoansesc@ufps.edu.co>
2025-05-19 00:38:12 -0500

    se intenta solucionar error

--- src/main/java/com/example/demo/SistemaConferenciasApplication.java
+++ src/main/java/com/example/demo/SistemaConferenciasApplication.java
@@ -2,10 +2,12 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EntityScan(basePackages = "com.example.demo.model")
 public class SistemaConferenciasApplication {
 
 	public static void main(String[] args) {
