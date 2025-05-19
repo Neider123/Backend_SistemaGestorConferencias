b0c4a0a88819a78cb752e04aea1fa1efa104d9c2
Neider123 <neiderjoansesc@ufps.edu.co>
2025-05-19 00:50:46 -0500

    se arregla el model conferencia para evitar consumo excesivo de memoria

--- src/main/java/com/example/demo/model/Conferencia.java
+++ src/main/java/com/example/demo/model/Conferencia.java
@@ -12,6 +12,7 @@ import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -25,11 +26,12 @@ import lombok.ToString;
 
 @Entity
 @Data
-@Table(name="conferencias")
+@Table(name = "conferencias")
 @ToString
-//@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id_conferencia")
+// @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class,
+// property = "id_conferencia")
 public class Conferencia implements Serializable {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id_conferencia;
@@ -39,52 +41,45 @@ public class Conferencia implements Serializable {
 	private String descripcion;
 	private String lugar;
 	private String estado;
-	
-	@OneToMany(mappedBy = "conferencia")
-	// Establecer referencia manejada
+
+	@OneToMany(mappedBy = "conferencia", fetch = FetchType.LAZY)
 	@JsonIgnore
 	@ToString.Exclude
 	private List<Topico> topicos;
-	
-	@ManyToOne
-	@JoinColumn(name = "id_chair")
-	private Usuario chair;
-	
-	
-	private String imagenUrl;
-	
-	@OneToMany(mappedBy = "conferencia")
+
+	@OneToMany(mappedBy = "conferencia", fetch = FetchType.LAZY)
 	@JsonIgnore
 	@ToString.Exclude
-    private List<Comite> comites;
-	
-	@OneToMany(mappedBy = "conferencia")
+	private List<Comite> comites;
+
+	@OneToMany(mappedBy = "conferencia", fetch = FetchType.LAZY)
 	@JsonIgnore
 	@ToString.Exclude
 	private List<Inscripcion> inscripciones;
-	
-	
-	@OneToMany(mappedBy = "conferencia", cascade = CascadeType.ALL)
-	@ToString.Exclude
+
+	@OneToMany(mappedBy = "conferencia", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
 	@JsonIgnore
+	@ToString.Exclude
 	private List<Convocatoria> convocatorias;
-	  
-	  
-	@OneToMany(mappedBy = "conferencia", cascade = CascadeType.ALL)
+
+	@OneToMany(mappedBy = "conferencia", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
 	@JsonIgnore
 	@ToString.Exclude
-	 private List<Precio> precios;
-	
-	@OneToMany(mappedBy = "conferencia")
+	private List<Precio> precios;
+
+	@OneToMany(mappedBy = "conferencia", fetch = FetchType.LAZY)
 	@JsonIgnore
 	@ToString.Exclude
 	private List<Sesion> sesiones;
-	
-	
+
+	@ManyToOne
+	@JoinColumn(name = "id_chair")
+	private Usuario chair;
+
+	private String imagenUrl;
+
 	@OneToOne
-    @JoinColumn(name = "id_cuenta_bancaria")
-    private CuentaBancaria cuentaBancaria;
-	
-	
+	@JoinColumn(name = "id_cuenta_bancaria")
+	private CuentaBancaria cuentaBancaria;
 
 }
